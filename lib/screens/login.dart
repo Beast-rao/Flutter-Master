9ea92ad107f1c875e3b7f0212f32bf10931decf3
Haier <faheemrao449@gmail.com>
2023-07-08 17:55:35 +0500

    DAY 2

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -4,6 +4,48 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Image.asset("assets/images/log.png",scale: 2.5,fit: BoxFit.contain,),
+          Text("Welcome ", style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText:"Username",
+                    labelText: "Name",
+
+                  ),
+                ),
+                SizedBox(height: 10,),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText:"Password",
+                    labelText: "Password",
+
+
+                  ),
+                  obscureText: true,
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(onPressed: (){
+            print("You Pressed Button");
+          }, child: Text("Login"),
+          style: TextButton.styleFrom(),)
+
+        ],
+      ),
+
+    );
   }
 }
